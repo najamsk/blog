c5aa3fa08ff8c80c4b7f1b014e34737259988cfa
najam awan <najamsk@gmail.com>
2023-06-11 20:32:31 +0300

    fixing config

--- tina/config.js
+++ tina/config.js
@@ -6,10 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  /* clientId: process.env.TINA_CLIENT_ID, // Get this from tina.io
-  token: process.env.TINA_TOKEN, // Get this from tina.io */
-  clientId: "bb3a329c-7a2f-4878-94be-957f3440bd69", // Get this from tina.io
-  token: "e4f94bdb9b5590824d23f51b24a1debe3d9d712e", // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
